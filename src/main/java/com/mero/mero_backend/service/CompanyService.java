b03f472b66f168d77f2260217385d1ba54d432f5
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-06-19 14:54:57 +0900

    Update CompanyService.java

--- src/main/java/com/mero/mero_backend/service/CompanyService.java
+++ src/main/java/com/mero/mero_backend/service/CompanyService.java
@@ -1,9 +1,13 @@
 package com.mero.mero_backend.service;
 
+import com.mero.mero_backend.converter.Encrypt;
 import com.mero.mero_backend.domain.entity.CompanyInfo;
 import com.mero.mero_backend.repository.CompanyRepository;
+import com.mero.mero_backend.repository.CompanySaltRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -12,12 +16,39 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
+
+    private final Encrypt encrypt;
     private final CompanyRepository companyRepository;
+    private final CompanySaltRepository companySaltRepository;
 
+    public int checkLoginIdDuplicate(String loginId) {
+        // 로그인 ID 조회
+        CompanyInfo companyInfo = companyRepository.findByLoginId(loginId);
 
+        // 로그인 ID가 존재하면 1, 존재하지 않으면 0 반환
+        return (companyInfo != null) ? 1 : 0;
+    }
+
+    @Transactional
     public CompanyInfo insertCompanyInfo(CompanyInfo companyInfo) {
+        // 비밀번호 암호화
+        String salt = encrypt.getSalt();
+        String encodedPw = encrypt.getEncrypt(String.valueOf(companyInfo.get("loginPw")), salt);
+
         // 고유 ID를 설정 (예: YYYYMMdd + 2자리 ID 생성 로직)
-        companyInfo.setCompanyId(generateCompanyInfoId());
+        String companyId = generateCompanyInfoId();
+        companyInfo.setCompanyId(companyId);
+        companyInfo.setLoginPw(encodedPw);
+        companyInfo.setCompanyType("USER");
+        companyInfo.setUseYn("Y");
+        companyInfo.setRegisterDateTime(LocalDate.now());
+        
+        // salt 정보 저장
+        CompanySalt companySalt = new CompanySalt();
+        companySalt.setCompanyId(companyId);
+        companySalt.setSalt(salt);
+        agencySaltRepository.save(agencySalt);
+        
         return companyRepository.save(companyInfo);
     }
 
