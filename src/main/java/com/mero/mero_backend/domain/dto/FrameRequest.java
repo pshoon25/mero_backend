1c5cb7d5a1ed45c26a4252f329d6521dbf743fb5
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 11:44:14 +0900

    Create FrameRequest.java

---
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameRequest.java
@@ -0,0 +1,23 @@
+package com.mero.mero_backend.domain.dto;
+
+import com.mero.mero_backend.domain.entity.CompanyInfo;
+import com.mero.mero_backend.domain.entity.Frame;
+import com.mero.mero_backend.domain.entity.DesignManagement;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FrameRequest {
+    private String companyId;
+    private String applicationType;
+    private String frameName;
+    private String useYn;
+
+    public FrameRequest(CompanyInfo companyInfo, DesignManagement designManagement, Frame frame) {
+        this.companyId = companyInfo.getCompanyId();
+        this.applicationType = designManagement.getApplicationType();
+        this.frameName = frame.getFrameName();
+        this.useYn = frame.getUseYn();
+    }
+}
