aeadb8d7d7e3e9ad06e9b0c7cf2a1ea3b873e182
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 14:19:13 +0900

    Update FrameRepository.java

--- src/main/java/com/mero/mero_backend/repository/FrameRepository.java
+++ src/main/java/com/mero/mero_backend/repository/FrameRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.repository.query.Param;
 public interface FrameRepository extends JpaRepository<Frame, Long> {
     @Query("SELECT COALESCE(CAST(MAX(CAST(SUBSTRING(f.frameId, 9, 2) AS INTEGER)) AS INTEGER), 0) FROM Frame f WHERE SUBSTRING(f.frameId, 1, 8) = :date")
     Integer findMaxIdByDate(@Param("date") String date);
+
+    @Query("SELECT f FROM Frame f JOIN FETCH f.designManagement")
+    List<Frame> findAllWithDesignManagement();
 }
