86a638da5a279f38f678ea70dcb5bc6ece1013f4
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 11:26:29 +0900

    Update FrameService.java

--- src/main/java/com/mero/mero_backend/service/FrameService.java
+++ src/main/java/com/mero/mero_backend/service/FrameService.java
@@ -1,15 +1,40 @@
 package com.mero.mero_backend.service;
 
 import com.mero.mero_backend.repository.FrameRepository;
+import com.mero.mero_backend.service.DesignService;
+import com.mero.mero_backend.domain.entity.Frame;
+import com.mero.mero_backend.domain.entity.DesignManagement;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import java.util.Map;
+import java.util.Objects;
+
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class FrameService {
     private final FrameRepository frameRepository;
+    private final DesignService designService;
 
+    @Transactional
+    public void saveFrame(MultipartFile file, Map<String, Object> requestMap) {
+        String companyId = String.valueOf(requestMap.get("companyId"));
+        DesignManagement designManagement = designService.uploadImage(file, companyId, "FRAME", null);
 
+        Frame frame = new Frame();
+        String frameId = generateFrameId();
+        frame.setFrameId(frameId);
+        frame.setDesignId(designManagement.getDesignId);
+        frame.setFrameName(String.valueOf(requsetMap.get("frameName")));
+        frame.setUseYn(String.valueOf(requsetMap.get("useYn")));
+        frameRepository.save(frame);
+    }   
+
+    public String generateFrameId() {
+        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        int maxId = frameRepository.findMaxIdByDate(today);
+        String formattedId = String.format("%02d", maxId + 1);
+        return today + formattedId;
+    }
 }
