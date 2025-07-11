12d86aac6b38989accc2f26e9a532a15a770eba1
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 14:45:47 +0900

    Update FrameController.java

--- src/main/java/com/mero/mero_backend/controller/FrameController.java
+++ src/main/java/com/mero/mero_backend/controller/FrameController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/frame")
