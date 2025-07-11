17922910c7dc5913dc0b4a7239b696b10a60bc6b
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 11:55:11 +0900

    Update FrameService.java

--- src/main/java/com/mero/mero_backend/service/FrameService.java
+++ src/main/java/com/mero/mero_backend/service/FrameService.java
@@ -8,6 +8,7 @@ import com.mero.mero_backend.domain.entity.DesignManagement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 import java.util.Objects;
