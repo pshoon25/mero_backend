49405d5c887bd143462246064f97fc500d0b7c14
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-11 14:45:33 +0900

    Update FrameService.java

--- src/main/java/com/mero/mero_backend/service/FrameService.java
+++ src/main/java/com/mero/mero_backend/service/FrameService.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
+import java.util.List;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
