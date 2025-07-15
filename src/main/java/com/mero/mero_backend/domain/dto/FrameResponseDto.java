b70b35c47ad5b7cbb5e4d0dcbe6fef9649b15392
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-15 16:32:42 +0900

    Update FrameResponseDto.java

--- src/main/java/com/mero/mero_backend/domain/dto/FrameResponseDto.java
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameResponseDto.java
@@ -1,26 +1,25 @@
 package com.mero.mero_backend.domain.dto;
 
 import com.mero.mero_backend.domain.entity.Frame;
-import com.mero.mero_backend.domain.entity.DesignManagement;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Getter 
 @Setter
 @NoArgsConstructor
 public class FrameResponseDto {
     private String frameId;
     private String frameName;
-    private String useYn;
+    private String useYn; 
     private String designId;
     private String designImageUrl;
 
-    public FrameResponseDto(Frame frame, String frameMngUseYn, DesignManagementDto designManagement) {
+    public FrameResponseDto(Frame frame, String frameMngUseYn, String designId, String designImageUrl) {
         this.frameId = frame.getFrameId();
         this.frameName = frame.getFrameName();
-        this.useYn = frameMngUseYn; 
-        this.designId = designManagement.getDesignId();
-        this.designImageUrl = designManagement.getDesignImageUrl();
+        this.useYn = frameMngUseYn;
+        this.designId = designId;       
+        this.designImageUrl = designImageUrl; 
     }
 }
