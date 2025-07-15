04ef2647c789ba9e18c9d181ce4c111043afb8bd
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-15 15:41:41 +0900

    Update FrameRequestDto.java

--- src/main/java/com/mero/mero_backend/domain/dto/FrameRequestDto.java
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameRequestDto.java
@@ -15,6 +15,8 @@ public class FrameRequestDto {
     private String companyId;
     private String applicationType;
     private String frameName;
+    private Integer frameWidth;
+    private Integer frameHeight;
     private String useYn;
 
     public FrameRequestDto(CompanyInfo companyInfo, DesignManagement designManagement, Frame frame) {
@@ -22,6 +24,8 @@ public class FrameRequestDto {
         this.companyId = companyInfo.getCompanyId();
         this.applicationType = designManagement.getApplicationType();
         this.frameName = frame.getFrameName();
+        this.frameWidth = frame.getFrameWidth();
+        this.frameHeight = frame.getFrameHeight();
         this.useYn = frame.getUseYn();
     }
 }
