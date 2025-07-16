bf9f487eb0d85928047c0fc8b04933a2bf120e04
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-16 10:06:17 +0900

    Update CtaRepository.java

--- src/main/java/com/mero/mero_backend/repository/CtaRepository.java
+++ src/main/java/com/mero/mero_backend/repository/CtaRepository.java
@@ -4,7 +4,7 @@ import com.mero.mero_backend.domain.entity.InquiryDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CtaRepository extends JpaRepository<InquiryDetails, Long> {
+public interface CtaRepository extends JpaRepository<InquiryDetails, String> {
     @Query("SELECT COALESCE(MAX(i.inquiryId), 0) FROM InquiryDetails i")
     Long findMaxInquiryId();
 }
