632a5ce5a977af0678d2cfbc9a732f59711304c4
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-16 13:57:23 +0900

    Update FrameManagementRepository.java

--- src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
+++ src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
@@ -13,16 +13,5 @@ public interface FrameManagementRepository extends JpaRepository<FrameManagement
     @Query("SELECT COALESCE(CAST(MAX(CAST(SUBSTRING(fm.frameMngId, 9, 2) AS INTEGER)) AS INTEGER), 0) FROM FrameManagement fm WHERE SUBSTRING(fm.frameMngId, 1, 8) = :date")
     Integer findMaxIdByDate(@Param("date") String date); 
 
-    @Query("SELECT new com.mero.mero_backend.domain.dto.FrameDesignResponseDto(fm, dm.designId) " +
-            "FROM FrameManagement fm " +
-            "JOIN FETCH fm.companyInfo ci " +
-            "JOIN FETCH fm.frame f " +
-            "LEFT JOIN DesignManagement dm ON dm.frameMngId = fm.frameMngId " +
-            "WHERE fm.frame.frameId = :frameId AND fm.companyInfo.companyId = :companyId")
-    List<FrameDesignResponseDto> findFrameDesignByFrameIdAndCompanyId(
-            @Param("frameId") String frameId,
-            @Param("companyId") String companyId
-    );
-
     Optional<FrameManagement> findByFrameIdAndCompanyId(String frameId, String companyId); 
 }
