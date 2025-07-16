d1703683ae05c3e8a0ffd60e1ed34d4a5746cad9
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-16 10:54:25 +0900

    Update FrameManagementRepository.java

--- src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
+++ src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
@@ -10,22 +10,19 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FrameManagementRepository extends JpaRepository<FrameManagement, String> {
-    @Query("SELECT COALESCE(CAST(MAX(CAST(SUBSTRING(fm.frameMngId, 9, 2) AS INTEGER)) AS INTEGER), 0) FROM FrameManagement fm WHERE SUBSTRING(fm.frame.frameId, 1, 8) = :date")
-    Integer findMaxIdByDate(@Param("date") String date);
+    @Query("SELECT COALESCE(CAST(MAX(CAST(SUBSTRING(fm.frameMngId, 9, 2) AS INTEGER)) AS INTEGER), 0) FROM FrameManagement fm WHERE SUBSTRING(fm.frameMngId, 1, 8) = :date")
+    Integer findMaxIdByDate(@Param("date") String date); 
 
     @Query("SELECT new com.mero.mero_backend.domain.dto.FrameDesignResponseDto(fm, dm.designId) " +
-           "FROM FrameManagement fm " +
-           "JOIN FETCH fm.companyInfo ci " +
-           "JOIN FETCH fm.frame f " +
-           "LEFT JOIN DesignManagement dm ON dm.frameMngId = fm.frameMngId " +
-           "WHERE fm.frame.frameId = :frameId AND fm.companyInfo.companyId = :companyId")
+            "FROM FrameManagement fm " +
+            "JOIN FETCH fm.companyInfo ci " +
+            "JOIN FETCH fm.frame f " +
+            "LEFT JOIN DesignManagement dm ON dm.frameMngId = fm.frameMngId " +
+            "WHERE fm.frame.frameId = :frameId AND fm.companyInfo.companyId = :companyId")
     List<FrameDesignResponseDto> findFrameDesignByFrameIdAndCompanyId(
             @Param("frameId") String frameId,
             @Param("companyId") String companyId
     );
 
-    Optional<FrameManagement> findByFrameIdAndCompanyId(String frameId, String companyId);
-
-    @Query("UPDATE FrameManagement fm SET fm.useYn = :useYn WHERE fm.frame.frameId = :frameId AND fm.companyInfo.companyId = :companyId")
-    int updateUseYnByFrameIdAndCompanyId(@Param("frameId") String frameId, @Param("companyId") String companyId, @Param("useYn") String useYn);
+    Optional<FrameManagement> findByFrame_FrameIdAndCompanyInfo_CompanyId(String frameId, String companyId); 
 }
