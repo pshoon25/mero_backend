1d80c6c7071da7cacf7b6a9b5e8321f4ec2b49c0
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-16 10:06:35 +0900

    Update ShootSettingsRepository.java

--- src/main/java/com/mero/mero_backend/repository/ShootSettingsRepository.java
+++ src/main/java/com/mero/mero_backend/repository/ShootSettingsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ShootSettingsRepository extends JpaRepository<ShootSettings, Long> {
+public interface ShootSettingsRepository extends JpaRepository<ShootSettings, String> {
     @Query("SELECT COALESCE(CAST(MAX(CAST(SUBSTRING(s.settingId, 9, 2) AS INTEGER)) AS INTEGER), 0) FROM ShootSettings s WHERE SUBSTRING(s.settingId, 1, 8) = :date")
     Integer findMaxIdByDate(@Param("date") String date);
 
