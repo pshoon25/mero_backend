cf70c97dff2609c616924bd43547820eae5ac34d
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-16 10:06:09 +0900

    Update CompanySaltRepository.java

--- src/main/java/com/mero/mero_backend/repository/CompanySaltRepository.java
+++ src/main/java/com/mero/mero_backend/repository/CompanySaltRepository.java
@@ -3,6 +3,6 @@ package com.mero.mero_backend.repository;
 import com.mero.mero_backend.domain.entity.CompanySalt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanySaltRepository extends JpaRepository<CompanySalt, Long> {
+public interface CompanySaltRepository extends JpaRepository<CompanySalt, String> {
     CompanySalt findByCompanyId(String companyId);
 }
