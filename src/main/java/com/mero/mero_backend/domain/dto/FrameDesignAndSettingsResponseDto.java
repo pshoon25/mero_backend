a8daab35c416fdee7982114517a2f5ab8449f3f9
박성훈 <pshoon25@naver.com>
2025-07-19 01:33:36 +0900

    Shoot Setting Add

--- src/main/java/com/mero/mero_backend/domain/dto/FrameDesignAndSettingsResponseDto.java
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameDesignAndSettingsResponseDto.java
@@ -1,5 +1,9 @@
 package com.mero.mero_backend.domain.dto;
 
+import com.mero.mero_backend.domain.entity.DesignManagement;
+import com.mero.mero_backend.domain.entity.Frame;
+import com.mero.mero_backend.domain.entity.FrameManagement;
+import com.mero.mero_backend.domain.entity.ShootSettings;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -12,31 +16,21 @@ public class FrameDesignAndSettingsResponseDto {
     private String frameId;
     private String companyId;
     private String designId; // DESIGN_MANAGEMENT에서 가져온 design_id
-    private Integer countdownTime; // shoot_settings
-    private Integer shotCount;     // shoot_settings
-    private Integer printCount;    // shoot_settings
+    private String countdownTime; // shoot_settings
+    private String shotCount;     // shoot_settings
+    private String printCount;    // shoot_settings
     private Integer frameWidth;    // frame
     private Integer frameHeight;   // frame
 
-    public FrameDesignAndSettingsResponseDto(
-            String frameMngId,
-            String frameId,
-            String companyId,
-            String designId,
-            Integer countdownTime,
-            Integer shotCount,
-            Integer printCount,
-            Integer frameWidth,
-            Integer frameHeight
-    ) {
-        this.frameMngId = frameMngId;
-        this.frameId = frameId;
-        this.companyId = companyId;
-        this.designId = designId;
-        this.countdownTime = countdownTime;
-        this.shotCount = shotCount;
-        this.printCount = printCount;
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
+    public FrameDesignAndSettingsResponseDto(FrameManagement frameManagement, DesignManagement designManagement, ShootSettings shootSettings, Frame frame) {
+        this.frameMngId = frameManagement.getFrameMngId();
+        this.frameId = frameManagement.getFrameId();
+        this.companyId = frameManagement.getCompanyId();
+        this.designId = designManagement.getDesignId();
+        this.countdownTime = shootSettings.getCountdownTime();
+        this.shotCount = shootSettings.getShotCount();
+        this.printCount =shootSettings.getPrintCount();
+        this.frameWidth = frame.getFrameWidth();
+        this.frameHeight = frame.getFrameHeight();
     }
 }
