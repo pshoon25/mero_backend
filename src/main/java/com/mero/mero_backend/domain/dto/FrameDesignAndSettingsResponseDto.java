497e2612420428e149312b8b47b086bdabf98067
박성훈 <pshoon25@naver.com>
2025-07-19 01:37:54 +0900

    Shoot Setting Add

--- src/main/java/com/mero/mero_backend/domain/dto/FrameDesignAndSettingsResponseDto.java
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameDesignAndSettingsResponseDto.java
@@ -1,36 +1,22 @@
 package com.mero.mero_backend.domain.dto;
 
-import com.mero.mero_backend.domain.entity.DesignManagement;
-import com.mero.mero_backend.domain.entity.Frame;
-import com.mero.mero_backend.domain.entity.FrameManagement;
-import com.mero.mero_backend.domain.entity.ShootSettings;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class FrameDesignAndSettingsResponseDto {
     private String frameMngId;
     private String frameId;
     private String companyId;
-    private String designId; // DESIGN_MANAGEMENT에서 가져온 design_id
-    private String countdownTime; // shoot_settings
-    private String shotCount;     // shoot_settings
-    private String printCount;    // shoot_settings
-    private Integer frameWidth;    // frame
-    private Integer frameHeight;   // frame
-
-    public FrameDesignAndSettingsResponseDto(FrameManagement frameManagement, DesignManagement designManagement, ShootSettings shootSettings, Frame frame) {
-        this.frameMngId = frameManagement.getFrameMngId();
-        this.frameId = frameManagement.getFrameId();
-        this.companyId = frameManagement.getCompanyId();
-        this.designId = designManagement.getDesignId();
-        this.countdownTime = shootSettings.getCountdownTime();
-        this.shotCount = shootSettings.getShotCount();
-        this.printCount =shootSettings.getPrintCount();
-        this.frameWidth = frame.getFrameWidth();
-        this.frameHeight = frame.getFrameHeight();
-    }
+    private String designId;
+    private String countdownTime;
+    private String shotCount;
+    private String printCount;
+    private Integer frameWidth;
+    private Integer frameHeight;
 }
