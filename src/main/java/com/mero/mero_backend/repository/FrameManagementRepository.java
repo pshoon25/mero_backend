475d6ec487fd2ebd70b814028981f30ca635d2c6
박성훈 <pshoon25@naver.com>
2025-07-19 14:38:03 +0900

    Type Change

--- src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
+++ src/main/java/com/mero/mero_backend/repository/FrameManagementRepository.java
@@ -42,7 +42,7 @@ public interface FrameManagementRepository extends JpaRepository<FrameManagement
             "LEFT JOIN FrameManagement fm ON fm.frameMngId = dm.frameMngId " +
             "LEFT JOIN FETCH fm.companyInfo ci " +
             "LEFT JOIN FETCH fm.frame f " +
-            "LEFT JOIN FETCH f.shootSettings ss " +
+            "LEFT JOIN ShootSettings ss ON ss.frameId = fm.frameId AND ss.companyId = fm.companyId " +
             "WHERE f.frameId = :frameId AND ci.companyId = :companyId")
     List<FrameDesignAndSettingsResponseDto> getFrameDesignAndSettingsByCompanyIdAndFrameId(
             @Param("frameId") String frameId,
