25ffa2f25d8300bb65d2a4ab462ff0bc283520fd
박성훈 <pshoon25@naver.com>
2025-07-22 00:15:07 +0900

    Media Results Add

--- src/main/java/com/mero/mero_backend/repository/MediaResultsRepository.java
+++ src/main/java/com/mero/mero_backend/repository/MediaResultsRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface MediaResultsRepository extends JpaRepository<MediaResults, String> {
     @Query(value = "SELECT COALESCE(MAX(CAST(SUBSTRING(mr.recordGroup, 10, 4) AS INTEGER)), 0) " +
-                   "FROM MediaResult mr " +
+                   "FROM MediaResults mr " +
                    "WHERE SUBSTRING(mr.recordGroup, 1, 8) = :date AND mr.companyId = :companyId")
     Integer getRecordGroup(@Param("date") String date, @Param("companyId") String companyId);
 
     @Query(value = "SELECT COALESCE(MAX(CAST(SUBSTRING(mr.resultId, 21, 4) AS INTEGER)), 0) " +
-                    "FROM MediaResult mr " +
+                    "FROM MediaResults mr " +
                     "WHERE SUBSTRING(mr.resultId, 1, 10) = :companyId AND SUBSTRING(mr.resultId, 12, 8) = :date")
     Integer findMaxSequenceIdByCompanyIdAndDate(@Param("companyId") String companyId, @Param("date") String date);
 }
