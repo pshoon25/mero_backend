bef8a10b8fa7ab9d8f994c2a5c2839c85659a471
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-21 15:06:07 +0900

    Update FrameResponseDto.java

--- src/main/java/com/mero/mero_backend/domain/dto/FrameResponseDto.java
+++ src/main/java/com/mero/mero_backend/domain/dto/FrameResponseDto.java
@@ -11,6 +11,17 @@ import lombok.NoArgsConstructor;
 public class FrameResponseDto {
     private String frameId;
     private String frameName;
+    private Integer frameWidth;
+    private Integer frameHeight;
+    private Integer frameCellCnt;
+    private Integer cellWidth;
+    private Integer cellHeight;
+    private Integer borderTop;
+    private Integer borderBottom;
+    private Integer borderLeft;
+    private Integer borderRight;
+    private Integer crossbarHorizontal;
+    private Integer crossbarVertical;
     private String useYn; 
     private String designId;
     private String designImageUrl;
@@ -18,6 +29,17 @@ public class FrameResponseDto {
     public FrameResponseDto(Frame frame, String frameMngUseYn, String designId, String designImageUrl) {
         this.frameId = frame.getFrameId();
         this.frameName = frame.getFrameName();
+        this.frameWidth = frame.getFrameWidth();
+        this.frameHeight = frame.getFrameHeight();
+        this.frameCellCnt = frame.getFrameCellCnt();
+        this.cellWidth = frame.getCellWidth();
+        this.cellHeight = frame.getCellHeight();
+        this.borderTop = frame.getBorderTop();
+        this.borderBottom = frame.getBorderBottom();
+        this.borderLeft = frame.getBorderLeft();
+        this.borderRight = frame.getBorderRight();
+        this.crossbarHorizontal = frame.getCrossbarHorizontal();
+        this.crossbarVertical = frame.getCrossbarVertical();
         this.useYn = frameMngUseYn;
         this.designId = designId;       
         this.designImageUrl = designImageUrl; 
@@ -26,7 +48,18 @@ public class FrameResponseDto {
     public FrameResponseDto(Frame frame) {
         this.frameId = frame.getFrameId();
         this.frameName = frame.getFrameName();
-        this.useYn = frame.getUseYn(); // Or assign a default like "UNKNOWN" or null
+        this.frameWidth = frame.getFrameWidth();
+        this.frameHeight = frame.getFrameHeight();
+        this.frameCellCnt = frame.getFrameCellCnt();
+        this.cellWidth = frame.getCellWidth();
+        this.cellHeight = frame.getCellHeight();
+        this.borderTop = frame.getBorderTop();
+        this.borderBottom = frame.getBorderBottom();
+        this.borderLeft = frame.getBorderLeft();
+        this.borderRight = frame.getBorderRight();
+        this.crossbarHorizontal = frame.getCrossbarHorizontal();
+        this.crossbarVertical = frame.getCrossbarVertical();
+        this.useYn = frame.getUseYn();
 
         if (frame.getDesignManagement() != null) {
             this.designId = frame.getDesignManagement().getDesignId();
