d328a604922452c355b47f22ce859c8fb2feabb5
박성훈 <116499170+pshoon25@users.noreply.github.com>
2025-07-22 17:10:17 +0900

    Create MedaiResultsResponseDto.java

---
+++ src/main/java/com/mero/mero_backend/domain/dto/MedaiResultsResponseDto.java
@@ -0,0 +1,29 @@
+package com.mero.mero_backend.domain.dto;
+
+import com.mero.mero_backend.domain.entity.MediaResults;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter 
+@Setter
+@NoArgsConstructor
+public class MedaiResultsResponseDto {
+    private String resultId;
+    private String companyId;
+    private String frameMngId;
+    private String designId;
+    private String recordGroup;
+    private String type;
+    private Date recordDateTime;
+
+    public MediaResultsDTO(MediaResults mediaResults) {
+        this.resultId = mediaResults.getResultId();
+        this.companyId = mediaResults.getCompanyId();
+        this.frameMngId = mediaResults.getFrameMngId();
+        this.designId = mediaResults.getDesignId();
+        this.recordGroup = mediaResults.getRecordGroup();
+        this.type = mediaResults.getType();
+        this.recordDateTime = mediaResults.getRecordDateTime();
+    }
+}
